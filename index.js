1b73212a2f7a4d4a945acb4ba64fa4d748eb1a76
Goldn7799 <syeifsultan@gmail.com>
2023-05-14 13:49:53 +0700

    credential

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const config = require('./config.json')
 // Main Command
 const runMain = async () => {
   console.log(`${color.cyan}`, 'Starting main sc..')
-  console.log(databases.getDb())
 }
 
 /// / Interface
@@ -16,6 +15,75 @@ app.use(cors({
   origin: config.bot.origin
 }))
 app.use('/', express.static('./public'))
+
+app.get('/user/:username/:password', (req, res) => {
+  const { username, password } = req.params
+  if (username && password) {
+    const users = databases.getUsers()
+    const emailList = Object.keys(users)
+    if (username.includes('@')) {
+      const user = users[username]
+      if (user) {
+        if (user.password === password) {
+          res.status(200).json({
+            success: true,
+            message: 'User Authenticated',
+            data: {
+              email: username,
+              user
+            }
+          })
+        } else {
+          res.status(402).json({
+            success: false,
+            message: 'User Password Wrong'
+          })
+        }
+      } else {
+        res.status(404).json({
+          success: false,
+          message: `User with email ${username} not found`
+        })
+      }
+    } else {
+      const usernameList = {}
+      for (const email of emailList) {
+        usernameList[users[email].username] = {
+          password: users[email].password,
+          email
+        }
+      }
+      const user = usernameList[username]
+      if (user) {
+        if (user.password === password) {
+          res.status(200).json({
+            success: true,
+            message: 'User Authenticated',
+            data: {
+              email: user.email,
+              user: users[user.email]
+            }
+          })
+        } else {
+          res.status(402).json({
+            success: false,
+            message: 'User Password Wrong'
+          })
+        }
+      } else {
+        res.status(404).json({
+          success: false,
+          message: `User with username ${username} not found`
+        })
+      }
+    }
+  } else {
+    res.status(403).json({
+      success: false,
+      message: 'Data not complete'
+    })
+  }
+})
 
 app.listen(config.bot.port, () => {
   console.log(`${color.bright + color.magenta}`, `Webserver Started at port ${color.underscore + config.bot.port}`)
--- public/main.js
+++ public/main.js
@@ -1,14 +1,27 @@
-//setup
-const root = document.getElementById('root');
+// setup
+const root = document.getElementById('root')
+const ip = location.hostname
+const port = location.port
+const ipUrl = `http://${ip}:${port}`
+console.log(ipUrl)
 
 // pages
 const pageState = {
-  login: false
+  login: false,
+  home: false
 }
 
+// function
+const logOut = () => {
+  localStorage.setItem('isLogin', 'false')
+  page.login()
+}
+
+// pages
 const page = {
-  login: ()=>{
-    pageState.login = true;
+  login: () => {
+    pageState.login = true
+    pageState.home = false
     root.innerHTML = `
       <div class="container">
         <div class="login">
@@ -21,45 +34,100 @@ const page = {
             <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
             <label for="floatingPassword">Password</label>
           </div>
-          <div class="invalid-feedback">
+          <div id="invalid" class="invalid-feedback">
             Please choose a username.
           </div>
-          <center><button style="margin-top: -60px; opacity: 0;" id="loginBtn"loginBtn>Login</button></center>
+          <center><button style="margin-top: -60px; opacity: 0;" id="loginBtn">Login</button></center>
         </div>
         <p class="copyright">© 2023 SGStudio Project</p>
       </div>
     `
-    const username = document.getElementById("floatingInput");
-    const password = document.getElementById("floatingPassword");
-    const passwordStyle = document.getElementById("pass");
-    const loginBtn = document.getElementById("loginBtn")
-    const loginRun = ()=>{
-      if(pageState.login){ 
-        if (`${username.value}`.length > 0){
-          passwordStyle.style.marginTop = "";
-          passwordStyle.style.opacity = "1";
-        }else {
-          passwordStyle.style.marginTop = "-60px";
-          passwordStyle.style.opacity = "0";
+    const username = document.getElementById('floatingInput')
+    const password = document.getElementById('floatingPassword')
+    const passwordStyle = document.getElementById('pass')
+    const loginBtn = document.getElementById('loginBtn')
+    const invalidMsg = document.getElementById('invalid')
+    const loginRun = () => {
+      if (pageState.login) {
+        if (`${username.value}`.length > 3) {
+          passwordStyle.style.marginTop = ''
+          passwordStyle.style.opacity = '1'
+        } else {
+          passwordStyle.style.marginTop = '-60px'
+          passwordStyle.style.opacity = '0'
         }
-        if (`${username.value}`.length > 0&&`${password.value}`.length > 0){
-          loginBtn.style.marginTop = "20px";
-          loginBtn.style.opacity = "1";
-        }else {
-          loginBtn.style.marginTop = "-60px";
-          loginBtn.style.opacity = "0";
+        if (`${username.value}`.length > 3 && `${password.value}`.length > 3) {
+          loginBtn.style.marginTop = '20px'
+          loginBtn.style.opacity = '1'
+        } else {
+          loginBtn.style.marginTop = '-60px'
+          loginBtn.style.opacity = '0'
         }
         setTimeout(() => {
-          loginRun();
-        }, 250);
+          loginRun()
+        }, 250)
       };
-    };
-    loginRun();
+    }
+    loginRun()
+    loginBtn.addEventListener('click', async () => {
+      loginBtn.innerHTML = '<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span> Login'
+      fetch(`${ipUrl}/user/${username.value}/${password.value}`, { method: 'GET' })
+        .then(rawRes => { return rawRes.json() })
+        .then(res => {
+          if (res.success) {
+            loginBtn.innerHTML = 'Login'
+            passwordStyle.classList.remove('is-invalid')
+            localStorage.setItem('username', username.value)
+            localStorage.setItem('password', password.value)
+            localStorage.setItem('isLogin', 'true')
+            page.home()
+          } else {
+            loginBtn.innerHTML = 'Login'
+            invalidMsg.innerText = ((username.value).includes('@')) ? 'Email or Password Wrong' : 'Username or Password Wrong'
+            passwordStyle.classList.add('is-invalid')
+          }
+        })
+        .catch(() => {
+          Notipin.Alert({
+            msg: 'Service Not Avabile', // Pesan kamu
+            yes: 'Ok', // Tulisan di tombol 'Yes'
+            onYes: () => { /* Kode di sini */ },
+            type: 'DANGER',
+            mode: 'DARK'
+          })
+        })
+    })
+  },
+  home: () => {
+    pageState.login = false
+    pageState.home = true
+    root.innerHTML = `
+      <div>
+        Home
+      </div>
+    `
   }
 }
 
 window.onload = () => {
   setTimeout(() => {
-    page.login();
-  }, 1000);
+    if (localStorage.getItem('isLogin') === 'true') {
+      fetch(`${ipUrl}/user/${localStorage.getItem('username')}/${localStorage.getItem('password')}`, { method: 'GET' })
+        .then(rawRes => { return rawRes.json() })
+        .then(res => {
+          if (res.success) {
+            localStorage.setItem('isLogin', 'true')
+            page.home()
+          } else {
+            localStorage.setItem('isLogin', 'false')
+            page.login()
+          }
+        })
+        .catch(() => {
+          page.login()
+        })
+    } else {
+      page.login()
+    }
+  }, 1000)
 }
